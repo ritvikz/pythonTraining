8d5f6bfbd81979a579efb5b42a4e6112032c25de
Ritvik <ritvikchouhan77@gmail.com>
2025-08-13 11:26:08 +0530

    Ineritance

---
+++ practice/Inheritance.py
@@ -0,0 +1,14 @@
+from Classes import Claculator
+
+
+class Claculator1(Claculator):
+    num2 = 200
+    def __init__(self):
+        Claculator.__init__(self, 1, 2)
+
+
+    def getCompleteData(self):
+        return self.num2 + self.num + self.Sum()
+
+obj = Claculator1()
+print(obj.getCompleteData())
